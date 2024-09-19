cf52cbc94739e056635bfbd30c9d047e57372ad2
CRUZ KWACHA <cruzkwacha@gmail.com>
2024-09-19 10:30:36 +0300

    Added basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+int_number1=10
+int_number2=5
+sum=int_number1 + int_number2
+subtraction=int_number1-int_number2
+multiplication=int_number1 * int_number2
+print("Addition of",int_number1, "and",int_number2,"is",sum)
+print("Subtraction of",int_number1, "and",int_number2,"is",subtraction)
+print("Multiplication of",int_number1, "and",int_number2,"is",multiplication)
