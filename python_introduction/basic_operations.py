a5e714322ac24950f4ab719238f6ec2efc27a33e
CRUZ KWACHA <cruzkwacha@gmail.com>
2024-09-19 13:06:30 +0300

    Correction

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,8 @@
 number1 = 10
 number2 = 5
-sum=int_number1 + int_number2
-subtraction=int_number1-int_number2
-multiplication=int_number1 * int_number2
-print("Addition of",int_number1, "and",int_number2,"is",sum)
-print("Subtraction of",int_number1, "and",int_number2,"is",subtraction)
-print("Multiplication of",int_number1, "and",int_number2,"is",multiplication)
+sum=number1 + number2
+subtraction=number1-number2
+multiplication=number1 * number2
+print("Addition of",number1, "and",number2,"is",sum)
+print("Subtraction of",number1, "and",number2,"is",subtraction)
+print("Multiplication of",number1, "and",number2,"is",multiplication)
