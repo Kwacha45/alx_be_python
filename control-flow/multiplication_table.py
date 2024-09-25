33ea4cb4cb5fc378c7e35d6888d2b7dc5599d836
CRUZ KWACHA <cruzkwacha@gmail.com>
2024-09-25 15:56:27 +0300

    added multiplication_table.py

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,6 @@
+from itertools import product
+
+number=int(input("Enter a number to see its multiplication table:"))
+for x in range(1,11):
+    product= number * x
+    print(f"{number} * {x} = {product}")
