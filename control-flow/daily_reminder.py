e2a072f1bd6934de0d040f8f44670d2bc63a1566
CRUZ KWACHA <cruzkwacha@gmail.com>
2024-09-27 09:11:17 +0300

    Changed variables

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
 Task=input("Enter the task description:")
-priority=input("Enter the task priority (high, medium, low):")
-Timebound=input("Is the task time-bound? (yes or no):")
+Priority=input("Enter the task priority (high, medium, low):")
+Time_bound=input("Is the task time-bound? (yes or no):")
 
 match Priority:
     case "high":
@@ -11,10 +11,10 @@ match Priority:
         print(f"Task '{Task}' is marked as LOW priority. Complete it when you have time.")
     case _:
         print(f"Task '{Task}' has an unrecognized priority. Defaulting to low priority.")
-if Timebound == "yes":
+if Time_bound == "yes":
     time_message = "that requires immediate attention today!"
 else:
     time_message = "which can be handled based on your availability."
 
 # Print the final reminder message
-print(f"\nReminder: Task '{task}' is a {priority_message} task {time_message}")
+print(f"\nReminder: Task '{Task}' is a {Priority} task {time_message}")
